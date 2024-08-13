987aa37d5e29a31c1354c899ede3ac6ffc4c1bff
elfarena <elfaaamez@gmail.com>
2024-08-12 18:57:51 -0700

    Add files via upload

---
+++ bab3/bab3soal1/src/main/java/com/mycompany/bab3soal1/Bab3soal1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab3soal1;
+
+/**
+ *
+ * @author RPL-SMKN 4 BOGOR
+ */
+public class Bab3soal1 {
+
+   public static void main(String[] arga) {
+        int Varl = 9;
+        float Var2 = 8.99f;
+        char Var3 = 'A';
+        boolean Var4 = false;
+        String Var5 = "Hello Word";
+        
+        System.out.println(Varl);
+        System.out.println(Var2);
+        System.out.println(Var3);
+        System.out.println(Var4);
+        System.out.println(Var5);
+        
+        
+}}
