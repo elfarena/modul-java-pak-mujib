a41112977939c2cb641c66f6d1fc69e42ad3df76
elfarena <elfaaamez@gmail.com>
2024-08-12 18:59:51 -0700

    Add files via upload

---
+++ bab4/bab4soal2/src/main/java/com/mycompany/bab4soal2/Bab4soal2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab4soal2;
+
+/**
+ *
+ * @author RPL-SMKN 4 BOGOR
+ */
+public class Bab4soal2 {
+
+    public static void main(String[] args) {
+        int angka1=5;
+        int angka2=6;
+        int angka3=8;
+        int angka4=8;
+        angka3++;
+        angka4++;
+ System.out.println("w = "+angka1);
+ System.out.println("x = "+angka2);
+ System.out.println("y = "+angka3);
+ System.out.println("z = "+angka4);
+ System.out.println("angka3 - x++ "+angka3);
+ System.out.println("angka4 - ++w "+angka4);
+ }
+}
+
