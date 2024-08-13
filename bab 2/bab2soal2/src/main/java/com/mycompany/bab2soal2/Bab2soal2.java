3eb5c441e8cf544bebac94ca24628693182b397a
elfarena <elfaaamez@gmail.com>
2024-08-12 18:55:38 -0700

    Add files via upload

---
+++ bab 2/bab2soal2/src/main/java/com/mycompany/bab2soal2/Bab2soal2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2soal2;
+
+/**
+ *
+ * @author RPL-SMKN 4 BOGOR
+ */
+public class Bab2soal2 {
+
+        public static String makanan;
+        public static final String minuman = "Jus Jambu";
+        public static void main(String[] args)  {
+            makanan = "Ayam Geprek";
+            
+            System.out.println("Makanan Favorite saya : " + makanan) ;
+            System.out.println("Minuman Favorite Saya : " + minuman) ;
+        }
+    }
+
---
+++ bab 2/bab2soall1/src/main/java/com/mycompany/bab2soall1/Bab2soall1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2soall1;
+
+/**
+ *
+ * @author RPL-SMKN 4 BOGOR
+ */
+public class Bab2soall1 {
+
+ public static void main (String[] args){
+    int a= 50;
+    float b= a;
+    
+    System.out.println (a);
+    System.out.println (b);
+}
+}
