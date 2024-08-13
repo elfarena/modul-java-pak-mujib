14844cf4b1a4c1c1d9bc1c5d04fe4601d172513f
elfarena <elfaaamez@gmail.com>
2024-08-12 19:04:59 -0700

    Add files via upload

---
+++ bab7/bab7soal1/src/bab7soal1/Bab7soal1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab7soal1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab7soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int day =2;
+    //Switch Expression
+     System.out.println("PILIH HARI =" );   
+     
+     switch (day) {
+         case 1: System.out.println("Sabtu ");
+            break;
+        
+        case 2: System.out.println("Minggu ");
+        break;
+        
+        default:System.out.println("Weekend");
+     }
+    }
+}
+    
+    
+
